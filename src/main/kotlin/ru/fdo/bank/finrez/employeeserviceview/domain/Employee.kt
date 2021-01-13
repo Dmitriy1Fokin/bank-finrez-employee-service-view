d0125730b148d837d9c4bd4d80004bbcecded5da
Dmitrii Fokin <fokinds1@gmail.com>
2021-01-14 00:43:40 +0300

    change val to var (lastName, firstName, middleName, position, dateOfDismissal, officeId)

--- src/main/kotlin/ru/fdo/bank/finrez/employeeserviceview/domain/Employee.kt
+++ src/main/kotlin/ru/fdo/bank/finrez/employeeserviceview/domain/Employee.kt
@@ -11,10 +11,10 @@ import javax.persistence.Table
 @Entity
 @Table(name = "employee")
 data class Employee(@Id @Column(name = "employee_id") val employeeId : String,
-                    @Column(name = "last_name") val lastName : String,
-                    @Column(name = "first_name") val firstName : String,
-                    @Column(name = "middle_name") val middleName : String?,
-                    @Column(name = "position") val position : String,
+                    @Column(name = "last_name") var lastName : String,
+                    @Column(name = "first_name") var firstName : String,
+                    @Column(name = "middle_name") var middleName : String?,
+                    @Column(name = "position") var position : String,
                     @Column(name = "date_of_hiring") val dateOfHiring : LocalDate,
-                    @Column(name = "date_of_dismissal") val dateOfDismissal : LocalDate?,
-                    @Column(name = "office_id") val officeId : String)
+                    @Column(name = "date_of_dismissal") var dateOfDismissal : LocalDate?,
+                    @Column(name = "office_id") var officeId : String)
