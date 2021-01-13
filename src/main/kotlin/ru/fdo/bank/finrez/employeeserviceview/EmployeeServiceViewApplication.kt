8fd36f5d1c2f233a7243f28f69ce34a66da7963a
Dmitrii Fokin <fokinds1@gmail.com>
2021-01-13 23:19:55 +0300

    first commit. init empty project

---
+++ src/main/kotlin/ru/fdo/bank/finrez/employeeserviceview/EmployeeServiceViewApplication.kt
@@ -0,0 +1,11 @@
+package ru.fdo.bank.finrez.employeeserviceview
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+open class EmployeeServiceViewApplication
+
+fun main(args: Array<String>) {
+    runApplication<EmployeeServiceViewApplication>(*args)
+}
