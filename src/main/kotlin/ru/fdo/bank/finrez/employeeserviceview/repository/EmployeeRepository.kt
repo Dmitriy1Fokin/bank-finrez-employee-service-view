4fed68ac2527002e66e5397d1342dafdd9fb00a7
Dmitrii Fokin <fokinds1@gmail.com>
2021-01-14 09:12:19 +0300

    added methods: findAllByOfficeId, findAllByOfficeIdIn

--- src/main/kotlin/ru/fdo/bank/finrez/employeeserviceview/repository/EmployeeRepository.kt
+++ src/main/kotlin/ru/fdo/bank/finrez/employeeserviceview/repository/EmployeeRepository.kt
@@ -1,7 +1,12 @@
 package ru.fdo.bank.finrez.employeeserviceview.repository
 
+import org.springframework.data.domain.Page
+import org.springframework.data.domain.Pageable
 import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor
 import ru.fdo.bank.finrez.employeeserviceview.domain.Employee
 
-interface EmployeeRepository : JpaRepository<Employee, String>, JpaSpecificationExecutor<Employee>
+interface EmployeeRepository : JpaRepository<Employee, String>, JpaSpecificationExecutor<Employee>{
+    fun findAllByOfficeId(officeId: String, pageable: Pageable) : Page<Employee>
+    fun findAllByOfficeIdIn(officeIds: List<String>, pageable: Pageable) : Page<Employee>
+}
